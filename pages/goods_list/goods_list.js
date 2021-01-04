54bee5492a1245811b3e882946b490e125ee9783
zongjianhui_ext <zongjianhui_ext@cop.cmsoft.com.cn>
2021-01-04 14:35:16 +0800

    商品列表

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -19,10 +19,20 @@ Page({
         isActive:false
       }
     ],
-    goodsList: []
+    goodsList:[]
   },
+  //接口要的参数
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize:10
+  },
+  //总页数
+  totalPages:1, //为默认值
   onLoad(options) { 
     console.log(options)
+    this.QueryParams.cid = options.cid
     this.getGoodsList()
   },
   // 点击tab切换
@@ -34,8 +44,55 @@ Page({
       tabs
     })
   },
-  // 请求商品列表数据
-  getGoodsList() { 
+  // 获取商品列表数据
+  async getGoodsList() { 
+    const res = await request({ url: "/goods/search", data: this.QueryParams });
+    const total = res.data.message.total;
+    console.log(res)
+    this.totalPages = Math.ceil(total/this.QueryParams.pagesize)   
+    // console.log(this.totalPages,'总页数');
+    this.setData({ //先解构原来的数据，再放请求回来的数据
+      goodsList:[...this.data.goodsList,...res.data.message.goods]
+    })
 
+    //关闭下拉刷新的窗口
+    wx.stopPullDownRefresh()
+
+  },
+  /**上拉加载
+   * 1.用户上滑页面，滚动条触底，页面加载下一条数据
+   *   1）找到滚动条触底事件
+   *   2）判断还有没有下一页数据
+   *        1)获取总页数和获取到当前的页码；当前页码pagenum、只有总条数，
+   *        总页数 = Math.ceil(总条数/页容量 pageSize)
+   *        2）判断当前的页码是否大于等于总页数，表示没有下一页  
+   *   3）假如 没有下一页数据，弹出一个提示框，如果有下一页数据，来加载数据
+   *        1）当前的页码++
+   *        2）重新发送请求，数据请求回来要对数组进行拼接，而不是全部替换
+  */
+  onReachBottom() {
+    if (this.QueryParams.pagenum >= this.totalPages) { 
+      //表示没有下一页
+      wx.showToast({
+        title: '已经到底了哦~'
+      });
+    } else {
+      this.QueryParams.pagenum++;
+      this.getGoodsList()
+    }
+  },
+  /**
+   * 下拉刷新
+   * 1.触发下拉刷新事件，需要在页面的json文件开启配置
+   * 2.重置数据数组
+   * 3.重置页码设置为1,重新发送请求
+   * 4.数据请求回来，需要手动关闭等待效果
+   */
+  onPullDownRefresh() { 
+    this.setData({
+      goodsList: []
+    })
+    this.QueryParams.pagenum = 1;
+    this.getGoodsList()
   }
 })
--- request/index.js
+++ request/index.js
@@ -1,4 +1,13 @@
+//同时发送异步代码的次数
+let ajaxTimes = 0; //针对一个页面三次请求接口
 export const request = (params) => { 
+    ajaxTimes++;//每调用一次方法，进行++
+    //显示loading
+    wx.showLoading({
+        title: "加载中",
+        mask:true
+    })
+
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
     return new Promise((resolve, reject) => { 
         wx.request({
@@ -9,6 +18,12 @@ export const request = (params) => {
             },
             fail: (res) => {
                 reject(res)
+            },
+            complete: () => { 
+                ajaxTimes--;
+                if (ajaxTimes ===0) { 
+                    wx.hideLoading();
+                }
             }
         }) 
     })
